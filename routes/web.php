564929ba473674b7828c24d961e8a255937db64a
Agta Fadjrin Aminullah <fadjrinagta@gmail.com>
2024-10-18 09:42:55 +0700

    Pengerjaan Praktikum 3 Jobsheet 8 (Tugas 3)

--- routes/web.php
+++ routes/web.php
@@ -112,6 +112,7 @@ Route::middleware(['auth'])->group(function() { //artinya semua route di dalam g
         Route::get('/barang/import', [BarangController::class, 'import']); // Ajax form upload excel
         Route::post('/barang/import_ajax', [BarangController::class, 'import_ajax']); // Ajax import excel
         Route::get('/barang/export_excel', [BarangController::class, 'export_excel']); // export excel
+        Route::get('/barang/export_pdf', [BarangController::class, 'export_pdf']); // export pdf
         
     });
     
